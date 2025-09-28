3767bee8a2b73a6892fc3dd1a16bdd8eae851779
aleksandra.zueva <aleksandra.zueva@exness.com>
2025-09-28 15:32:51 +0300

    add Db

---
+++ app/src/main/java/ru/practicum/android/diploma/db/data/impl/VacancyRepositoryDbImpl.kt
@@ -0,0 +1,27 @@
+package ru.practicum.android.diploma.db.data.impl
+
+import androidx.room.Room
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import ru.practicum.android.diploma.App.Companion.appContext
+import ru.practicum.android.diploma.db.data.AppDatabase
+import ru.practicum.android.diploma.db.data.dao.VacancyDao
+import ru.practicum.android.diploma.db.data.entity.VacancyEntity
+import ru.practicum.android.diploma.db.domain.VacancyRepositoryDb
+
+class VacancyRepositoryDbImpl : VacancyRepositoryDb {
+
+    private val roomDb = Room.databaseBuilder(appContext, AppDatabase::class.java, "database.db")
+        .build()
+
+    private val vacancyDao: VacancyDao = roomDb.getVacancyDao()
+
+    override suspend fun deleteVacancy(vacancyId: String) {
+        vacancyDao.deleteVacancy(vacancyId)
+    }
+
+    override suspend fun getVacancies(): Flow<List<VacancyEntity>?> = flow {
+        val vacanciesEntityList = vacancyDao.getVacancies()
+        emit(vacanciesEntityList)
+    }
+}
