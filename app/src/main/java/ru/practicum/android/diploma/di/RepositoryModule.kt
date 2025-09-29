1a83575dfbb9ebef9db3013c73976471ecde3743
Ruslan Fazlyev <rifazlyev@gmail.com>
2025-09-29 23:11:34 +0300

    фикс детекта

--- app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
@@ -3,8 +3,8 @@ package ru.practicum.android.diploma.di
 import org.koin.dsl.module
 import ru.practicum.android.diploma.db.data.impl.VacancyRepositoryImpl
 import ru.practicum.android.diploma.db.domain.VacancyRepository
-import ru.practicum.android.diploma.network.domain.VacancyRepository as VacancyRepositoryNetwork
 import ru.practicum.android.diploma.network.data.impl.VacancyRepositoryImpl as VacancyRepositoryNetworkImpl
+import ru.practicum.android.diploma.network.domain.VacancyRepository as VacancyRepositoryNetwork
 
 val repositoryModule = module {
     single<VacancyRepository> { VacancyRepositoryImpl(get()) }
