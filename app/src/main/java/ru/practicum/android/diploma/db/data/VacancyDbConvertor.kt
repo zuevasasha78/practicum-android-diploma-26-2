7dd87867ac5b3d277715a6a44bbcee8b953c31ce
Ruslan Fazlyev <rifazlyev@gmail.com>
2025-10-07 17:08:14 +0300

    метод загрузки вакансий из бд

--- app/src/main/java/ru/practicum/android/diploma/db/data/VacancyDbConvertor.kt
+++ app/src/main/java/ru/practicum/android/diploma/db/data/VacancyDbConvertor.kt
@@ -1,7 +1,21 @@
 package ru.practicum.android.diploma.db.data
 
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import ru.practicum.android.diploma.db.data.entity.VacancyEntity
+import ru.practicum.android.diploma.network.domain.models.Salary
+import ru.practicum.android.diploma.network.domain.models.Vacancy
 
 class VacancyDbConvertor(private val gson: Gson) {
-    // todo
+    fun map(vacancyEntity: VacancyEntity): Vacancy {
+        val type = object : TypeToken<Salary>() {}.type
+        val salary = gson.fromJson<Salary>(vacancyEntity.salary, type)
+        return Vacancy(
+            id = vacancyEntity.vacancyId.toString(),
+            name = vacancyEntity.name,
+            employerName = vacancyEntity.companyName,
+            salaryDto = salary,
+            employerLogo = vacancyEntity.employerLogo
+        )
+    }
 }
