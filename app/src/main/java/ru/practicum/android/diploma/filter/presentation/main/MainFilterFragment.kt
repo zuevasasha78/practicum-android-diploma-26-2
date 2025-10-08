dc0062e875dd9be272f1c4a1ffd9327e1db3732b
Ruslan Fazlyev <rifazlyev@gmail.com>
2025-10-09 01:10:04 +0300

    сверстал текст-инпут место работы

--- app/src/main/java/ru/practicum/android/diploma/filter/presentation/main/MainFilterFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/presentation/main/MainFilterFragment.kt
@@ -1,9 +1,11 @@
 package ru.practicum.android.diploma.filter.presentation.main
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -30,15 +32,40 @@ class MainFilterFragment : Fragment() {
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
-        binding.placeChooserButton.setOnClickListener {
-            findNavController().navigate(R.id.action_mainFilterFragment_to_workPlaceFragment)
+        binding.toolbarFilter.setNavigationOnClickListener {
+            findNavController().navigateUp()
         }
+
+        binding.placeEditText.setOnClickListener {
+            binding.placeEditText.setText("Москва")
+            binding.placeInputLayout.setEndIconDrawable(R.drawable.icon_close)
+            updateHintColor(true)
+            //findNavController().navigate(R.id.action_mainFilterFragment_to_workPlaceFragment)
+        }
+
+        binding.placeInputLayout.setEndIconOnClickListener {
+            binding.placeEditText.setText("")
+            binding.placeInputLayout.setEndIconDrawable(R.drawable.leading_icon)
+            updateHintColor(false)
+        }
+
         binding.sectorChooserButton.setOnClickListener {
             findNavController().navigate(
                 R.id.action_mainFilterFragment_to_chooserFragment,
                 bundleOf(ARG_NAME to ChooserType.SectorType),
             )
         }
+    }
+
+
+
+
+
+    private fun updateHintColor(hasText: Boolean) {
+        val colorResId = if (hasText) R.color.text_color else R.color.hint_color_filter_screen
+        binding.placeInputLayout.defaultHintTextColor = ColorStateList.valueOf(
+            ContextCompat.getColor(requireContext(), colorResId)
+        )
     }
 
     override fun onDestroyView() {
