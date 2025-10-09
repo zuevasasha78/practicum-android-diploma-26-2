fb526822e3560aa7c21eb56504721fc84b6f2f00
Ruslan Fazlyev <rifazlyev@gmail.com>
2025-10-09 21:13:56 +0300

    логика и работа с ui во фрагменте

--- app/src/main/java/ru/practicum/android/diploma/filter/presentation/main/MainFilterFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/presentation/main/MainFilterFragment.kt
@@ -1,23 +1,26 @@
 package ru.practicum.android.diploma.filter.presentation.main
 
+import android.content.Context
 import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.core.content.ContextCompat
-import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.textfield.TextInputLayout
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentMainFilterBinding
-import ru.practicum.android.diploma.filter.domain.ChooserType
-import ru.practicum.android.diploma.filter.presentation.chooser.ChooserFragment.Companion.ARG_NAME
 
 class MainFilterFragment : Fragment() {
     private var _binding: FragmentMainFilterBinding? = null
     private val binding get() = _binding!!
+    private val mainFilterViewModel by viewModel<MainFilterViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,29 +40,37 @@ class MainFilterFragment : Fragment() {
             findNavController().navigateUp()
         }
 
-        binding.placeEditText.setOnClickListener {
-            binding.placeEditText.setText("Москва")
+        mainFilterViewModel.buttonsVisible.observe(viewLifecycleOwner) { isVisible ->
+            isButtonsApplyAndResetVisible(isVisible)
+        }
 
+        mainFilterViewModel.filters.observe(viewLifecycleOwner) { filterUiState ->
+            render(filterUiState)
+        }
+
+        binding.placeEditText.setOnClickListener {
+            // Заглушка для теста, удалится после реализации экрана "Место работы"
+            val text = "Москва"
+            mainFilterViewModel.onPlaceChanged(text)
             binding.placeInputLayout.apply {
-                setEndIconDrawable(R.drawable.icon_close)
-                updateHintColor(this, true)
+                updateFieldState(this, true)
             }
             //findNavController().navigate(R.id.action_mainFilterFragment_to_workPlaceFragment)
         }
 
         binding.placeInputLayout.setEndIconOnClickListener {
-            binding.placeEditText.setText("")
+            mainFilterViewModel.onPlaceChanged("")
             binding.placeInputLayout.apply {
-                setEndIconDrawable(R.drawable.leading_icon)
-                updateHintColor(this, false)
+                updateFieldState(this, false)
             }
         }
 
         binding.industryEditText.setOnClickListener {
-            binding.industryEditText.setText("IT")
+            // Заглушка для теста, удалится после реализации экрана "Отрасль"
+            val text = "IT"
+            mainFilterViewModel.onIndustryChanged(text)
             binding.industryInputLayout.apply {
-                setEndIconDrawable(R.drawable.icon_close)
-                updateHintColor(this, true)
+                updateFieldState(this, true)
             }
 //            findNavController().navigate(
 //                R.id.action_mainFilterFragment_to_chooserFragment,
@@ -68,19 +79,99 @@ class MainFilterFragment : Fragment() {
         }
 
         binding.industryInputLayout.setEndIconOnClickListener {
-            binding.industryEditText.setText("")
+            mainFilterViewModel.onIndustryChanged("")
             binding.industryInputLayout.apply {
-                setEndIconDrawable(R.drawable.leading_icon)
-                updateHintColor(this, false)
+                updateFieldState(this, false)
             }
+        }
+
+        binding.salaryEditText.setOnFocusChangeListener { _, _
+            ->
+            updateSalaryField()
+        }
+
+        binding.root.setOnClickListener {
+            hideKeyboardAndClearFocus()
+        }
+
+        binding.salaryEditText.addTextChangedListener { text ->
+            val textString = text.toString()
+            if (textString != mainFilterViewModel.filters.value?.salary) {
+                updateSalaryField()
+                mainFilterViewModel.onSalaryChanged(textString)
+            }
+        }
+
+        binding.salaryInputLayout.setEndIconOnClickListener {
+            mainFilterViewModel.onSalaryChanged("")
+            updateSalaryField()
+        }
+
+        binding.onlyWithSalaryCheckbox.setOnCheckedChangeListener { _, isChecked ->
+            mainFilterViewModel.onOnlyWithSalaryChanged(isChecked)
+        }
+
+        binding.applyButton.setOnClickListener {
+            findNavController().navigate(
+                R.id.action_mainFilterFragment_to_searchFragment
+            )
+        }
+
+        binding.resetButton.setOnClickListener {
+            resetFilter()
         }
     }
 
-    private fun updateHintColor(textInputLayout: TextInputLayout, hasText: Boolean) {
+    private fun updateFieldState(textInputLayout: TextInputLayout, hasText: Boolean) {
         val colorResId = if (hasText) R.color.text_color else R.color.hint_color_filter_screen
         textInputLayout.defaultHintTextColor = ColorStateList.valueOf(
             ContextCompat.getColor(requireContext(), colorResId)
         )
+        val iconRes = if (hasText) R.drawable.icon_close else R.drawable.leading_icon
+        textInputLayout.setEndIconDrawable(iconRes)
+    }
+
+    private fun updateSalaryField() {
+        val hasFocus = binding.salaryEditText.hasFocus()
+        val hasText = binding.salaryEditText.text?.isNotEmpty() == true
+        val colorResId = when {
+            hasFocus -> R.color.blue  // При фокусе - синий
+            hasText -> R.color.black_universal  // Заполнено без фокуса - черный
+            else -> R.color.salary_text_hint  // Пустое без фокуса - серый/белый в зависимости от темы
+        }
+        binding.salaryInputLayout.defaultHintTextColor = ColorStateList.valueOf(
+            ContextCompat.getColor(requireContext(), colorResId)
+        )
+
+        binding.salaryInputLayout.setEndIconDrawable(R.drawable.icon_close)
+        binding.salaryInputLayout.isEndIconVisible = hasText
+    }
+
+    private fun hideKeyboardAndClearFocus() {
+        val imm = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(binding.root.windowToken, 0)
+        binding.salaryEditText.clearFocus()
+    }
+
+    private fun isButtonsApplyAndResetVisible(isVisible: Boolean) {
+        binding.applyButton.isVisible = isVisible
+        binding.resetButton.isVisible = isVisible
+    }
+
+    private fun render(filterUIState: FilterUiState) {
+        binding.placeEditText.setText(filterUIState.place)
+        binding.industryEditText.setText(filterUIState.industry)
+        if (binding.salaryEditText.text.toString() != filterUIState.salary) {
+            binding.salaryEditText.setText(filterUIState.salary)
+        }
+        binding.onlyWithSalaryCheckbox.isChecked = filterUIState.onlyWithSalary
+    }
+
+    private fun resetFilter() {
+        mainFilterViewModel.reset()
+        updateFieldState(binding.placeInputLayout, hasText = false)
+        updateFieldState(binding.industryInputLayout, hasText = false)
+        updateSalaryField()
     }
 
     override fun onDestroyView() {
